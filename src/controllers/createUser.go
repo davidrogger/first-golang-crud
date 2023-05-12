8c9b0951c14b1701064831a453576aaf19613e7f
davidrogger <davidrogger.a@gmail.com>
2023-05-12 15:25:03 -0300

    FEAT: validation new format

--- src/controllers/createUser.go
+++ src/controllers/createUser.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/davidrogger/first-golang-crud/src/configurations/rest_err"
+	"github.com/davidrogger/first-golang-crud/src/configurations/validation"
 	"github.com/davidrogger/first-golang-crud/src/controllers/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +16,7 @@ func CreateUser(c *gin.Context) {
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		message := fmt.Sprintf("Incorrect Field, error=%s\n", err.Error())
 		log.Print(message)
-		restErr := rest_err.NewBadRequestError(message)
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
