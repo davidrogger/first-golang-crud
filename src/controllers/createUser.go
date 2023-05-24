5bcc4b16a39e2a77c49e1b2385cf8e94ff829698
davidrogger <davidrogger.a@gmail.com>
2023-05-24 11:35:56 -0300

    FEAT: added initializing application log

--- src/controllers/createUser.go
+++ src/controllers/createUser.go
@@ -2,23 +2,44 @@ package controller
 
 import (
 	"fmt"
-	"log"
+	"net/http"
 
+	"github.com/davidrogger/first-golang-crud/src/configurations/logger"
 	"github.com/davidrogger/first-golang-crud/src/configurations/validation"
 	"github.com/davidrogger/first-golang-crud/src/controllers/model/request"
+	"github.com/davidrogger/first-golang-crud/src/controllers/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap/zapcore"
 )
 
 func CreateUser(c *gin.Context) {
-	log.Println("Init CreateUser controller")
+	LogField := zapcore.Field{
+		Key:    "journey",
+		String: "createUser",
+	}
+
+	// outra opção zap.String("jorney", "createUser") para definir o campo de log
+
+	logger.Info("Init CreateUser controller", LogField)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		message := fmt.Sprintf("Incorrect Field, error=%s\n", err.Error())
-		log.Print(message)
+		logger.Error(message, err, LogField)
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
+
+	logger.Info("User Created successfully", LogField)
+
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(http.StatusOK, response)
 
 }
